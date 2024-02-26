62d5f7fc4461cc794a85fba84c1a5ec5bbb7b057
Brandon Mazurkiewicz <boomerbash962@gmail.com>
2024-02-26 10:59:58 -0500

    Header information added

---
+++ ftp_client.java
@@ -0,0 +1,10 @@
+/*
+ftp_client.java
+
+Client side protocol for multithreaded server application
+
+By: Hannah S., Gabi M., Saif F., Brandon M.
+Last updated: 
+*/
+
+//Stuff
---
+++ ftp_server.java
@@ -0,0 +1,10 @@
+/*
+ftp_server.java
+
+Server side protocol for multithreaded server application.
+
+By: Hannah S., Gabi M., Said F., Brandon M.
+Last updated: 
+*/
+
+//Stuff
